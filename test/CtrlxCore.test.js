a26dc52e1dc23849a64b03a7c77f80af62dd655b
Sebastian Krauskopf <mail@sebakrau.de>
2020-05-07 23:03:36 +0200

    Improve error handling by introducing a dedicated CtrlxProblemError for API errors.

--- test/CtrlxCore.test.js
+++ test/CtrlxCore.test.js
@@ -11,7 +11,7 @@ const TEST_CONTROL_PASSWORD ='boschrexroth';
 
 describe('Simple tests with real control', () => {
 
-  it('should return true when READing framework/bundles/com_boschrexroth_comm_datalayer/active', (done) => {
+  it('should return true when reading framework/bundles/com_boschrexroth_comm_datalayer/active', (done) => {
 
     let ctrlx = new CtrlxCore(TEST_CONTROL_HOSTNAME, TEST_CONTROL_USERNAME, TEST_CONTROL_PASSWORD);
 
@@ -23,16 +23,16 @@ describe('Simple tests with real control', () => {
 
   });
 
-  it('should read two values and then logout without error', () => {
+  it('should read two values and then logout without error', (done) => {
 
     let ctrlx = new CtrlxCore(TEST_CONTROL_HOSTNAME, TEST_CONTROL_USERNAME, TEST_CONTROL_PASSWORD);
 
     ctrlx.logIn()
       .then(() => ctrlx.readDatalayer('framework/bundles/com_boschrexroth_comm_datalayer/active') )
-      .then((data) => console.log(data))
+      .then((data) => {/*console.log(data)*/})
       .then(() => ctrlx.readDatalayer('framework/metrics/system/cpu-utilisation-percent') )
-      .then((data) => console.log(data))
-      .catch((err) => console.error('Housten we are in trouble: ' + err))
+      .then((data) => {done(); /*console.log(data)*/})
+      .catch((err) => done(err))
       .finally(() => ctrlx.logOut());
 
   });
@@ -43,14 +43,33 @@ describe('Simple tests with real control', () => {
 
     ctrlx.logIn()
       .then(() => ctrlx.readDatalayerMetadata('framework/bundles/com_boschrexroth_comm_datalayer/active') )
-      .then((data) => console.log(data))
+      .then((data) => {/*console.log(data)*/})
       .then(() => ctrlx.readDatalayerMetadata('framework/metrics/system/cpu-utilisation-percent') )
-      .then((data) => console.log(data))
+      .then((data) => {/*console.log(data)*/})
       .catch((err) => console.error('Housten we are in trouble: ' + err))
       .finally(() => ctrlx.logOut());
 
   });
+});
 
 
+
+describe('Check correct error handling', () => {
+
+  it('should return an error', (done) => {
+
+    let ctrlx = new CtrlxCore(TEST_CONTROL_HOSTNAME, TEST_CONTROL_USERNAME, TEST_CONTROL_PASSWORD);
+
+    ctrlx.logIn()
+      .then(() => ctrlx.readDatalayerMetadata('invalidnode') )
+      .then((data) => done(new Error("should not reach this code. Expected error instead of: " + JSON.stringify(data))))
+      .catch((err) => {
+          assert.equal(err.name, 'CtrlxProblemError');
+          console.log('Received excpected error object for unknown node in Data Layer: ' + err);
+          done();
+        })
+      .finally(() => ctrlx.logOut());
+
+  });
 
 });
